d1083bf3397d37393292e4fb276268814fdd8755
shubham potadar <spotadar.sp@gmail.com>
2022-05-17 11:09:32 +0530

    added pipee directive and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { UserComponent } from './user/user.component';
+import { ImgurlPipe } from './imgurl.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainContainerComponent,
+    ListUserComponent,
+    UserComponent,
+    ImgurlPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
